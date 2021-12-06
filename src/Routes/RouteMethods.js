4dce52408b4fe889891b743c96abb3f4663fb345
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-06 12:01:17 -0500

    Created endpoint for searching for a post by Id. (VPP)

--- src/Routes/RouteMethods.js
+++ src/Routes/RouteMethods.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const{Reply, Post, validateComment}=require("../Models/Comment");
+const{Reply, Post, validateComment}=require("../models/Comment");
 const router=express.Router();
 
 router.get("/:postID", async (req, res) => {
@@ -17,15 +17,39 @@ router.post('/', async (req, res) => {
         if (error)
             return res.status(400).send(error);
 
-        const comment = new Post({
+        const post = new Post({
             postId: req.body.postId,
             text: req.body.text,
         });
 
-        await comment.save();
+        await post.save();
 
-        return res.send(comment);
-    }   catch (ex)
-})
+        return res.send(post);
+    }   catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
+
+router.put('/:postId', async (req, res) => {
+    try {
+
+        const post = await Post.findByIdAndUpdate(
+            req.params.postId,
+            {
+                ...req.body
+            },
+            {new:true}
+        );
+
+        if (!post)
+            return res.status(400).send(`The post requested does not exist.`)
+
+        await post.save();
+
+        return res.send(post);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`)
+    }
+});
 
 module.exports=router;
