2951aa1670df1215dffc877f872a520b0b6066a9
victorPaturzo <93002935+victorPaturzo@users.noreply.github.com>
2021-12-06 11:27:59 -0500

    Merge. (VPP)

--- src/Routes/RouteMethods.js
+++ src/Routes/RouteMethods.js
@@ -11,4 +11,21 @@ router.get("/:postID", async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try {
+        const {error} = validateComment(req.body);
+        if (error)
+            return res.status(400).send(error);
+
+        const comment = new Post({
+            postId: req.body.postId,
+            text: req.body.text,
+        });
+
+        await comment.save();
+
+        return res.send(comment);
+    }   catch (ex)
+})
+
 module.exports=router;
